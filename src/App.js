92f74ceb96988d88ef4b20e31e90217f8b3f6646
thiagofdiniz <thiago.diniz2@etec.sp.gov.br>
2020-02-07 20:29:44 -0300

    up

--- src/App.js
+++ src/App.js
@@ -4,14 +4,35 @@ import './App.css';
 
 function App() {
   return (
-    <div class="ui menu inverted fixed">
-      <a class="item">Editorials</a>
-      <a class="item">Reviews</a>
-      <div class="ui icon input">
-        <input type="text" placeholder="Buscar..." />
-        <i aria-hidden="true" class="search icon"></i>
+    <>
+      <div class="ui menu inverted fixed">
+        <a class="item">Editorials</a>
+        <a class="item">Reviews</a>
+        <div class="ui icon input">
+          <input type="text" placeholder="Buscar..." />
+          <i aria-hidden="true" class="search icon"></i>
+        </div>
       </div>
-    </div>
+
+      <div class="ui grid three column container">
+        <div class="column">
+          <div class="ui card">
+            <div class="image"><img src="/images/avatar/large/matthew.png" /></div>
+            <div class="content">
+              <div class="header">Matthew</div>
+              <div class="meta"><span class="date">Joined in 2015</span></div>
+              <div class="description">Matthew is a musician living in Nashville.</div>
+            </div>
+            <div class="extra content">
+              <a>
+                <i aria-hidden="true" class="user icon"></i>
+                22 Friends
+    </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
